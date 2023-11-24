4644c4d43bf076ae9a09170d9de7a9b9d38521e2
abushahed670 <abu.shahed670@gmail.com>
2023-11-25 02:15:08 +0600

    even and odd

---
+++ even.js
@@ -0,0 +1,20 @@
+// var number = 0;
+// while(number < 10){
+//     console.log(number)
+//     number ++;
+// }
+// // to run 1 to 10
+
+// var number = 1;
+// while(number <= 10){
+//     console.log(number)
+//     number ++;
+// }
+
+// to show even number
+var number = 0;
+while(number <= 10){
+    console.log(number)
+    number = number +2;
+}
+
