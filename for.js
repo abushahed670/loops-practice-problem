2b3e5d2d152443e9d9c95a7cae096eff624f4d53
abushahed670 <abu.shahed670@gmail.com>
2023-11-25 02:43:30 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,26 @@
+//while loop
+//  var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log(' Roast den, please!')
+//     console.log(roastGiven)
+//     roastGiven ++;
+// }
+
+//  for loop
+for(var roastGiven = 0; roastGiven < 9; roastGiven++){
+    console.log('roast den, please')
+}
+
+// for loop r while loop same but likar system ekto vinno
+// simple version of for loop
+for (i = 0; i <7; i++){
+    console.log(i)
+}
+// to show even number
+for (i = 0; i <=20; i+=2){
+    console.log(i)
+}
+// to show odd number
+for (i = 1; i <7; i+=2){
+    console.log(i)
+}
