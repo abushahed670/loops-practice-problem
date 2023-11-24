9add614db94aff879dd57f952ce6a220b77aa0bf
abushahed670 <abu.shahed670@gmail.com>
2023-11-24 23:46:52 +0600

    rivision previous class

---
+++ revision.js
@@ -0,0 +1,33 @@
+// string type variable
+var bottleFor = 'water';
+
+// number type variable
+
+var glassPrice = 120;
+
+// boolean type variable
+
+var isExpensive = true,
+
+// items type variable 
+
+var tableItems = ['rice', 'dall', 'vat', 'fish', 'meat'];
+ var items3 = tableItems[3];
+ tableItems[4] = 'ilish';
+ var vatIndex = tableItems.indexOf('vat');
+
+//  push/po
+
+// conditional... if, else, else if
+
+if(tableItems > 4){
+
+
+}
+else if(tableItems[3] == vat){
+
+}
+else{
+
+}
+
