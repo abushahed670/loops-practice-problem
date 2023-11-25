1be075e495f6b610d67d3912e7062558873e7853
abushahed670 <abu.shahed670@gmail.com>
2023-11-26 01:40:19 +0600

    reverse way both for and while loop

---
+++ reverse.js
@@ -0,0 +1,28 @@
+// normal way 1 to 10
+
+var number = 1;
+while(number <= 10){
+    console.log(number)
+   number++;
+}
+
+// //mechanism of while loop into  reverse way as 10 t0 1
+
+var num = 10;
+while (num >= 1){
+    console.log(num)
+    num --;
+}
+
+
+// normal way of for loop
+
+for ( var i=1; i<10; i++){
+    console.log(i)
+}
+
+// reverse way of for loop
+
+for( var i=10; i >= 1; i--){
+    console.log(i)
+}
