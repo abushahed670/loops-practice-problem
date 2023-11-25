176b5c81381b872d322a0cf67a3f0f470bbd78df
abushahed670 <abu.shahed670@gmail.com>
2023-11-26 00:32:58 +0600

    array loop added

---
+++ array-loop.js
@@ -0,0 +1,17 @@
+for( i= 0; i <= 10; i++){
+    console.log(i)
+}
+
+// target: display every elements of an array
+
+var numbers = [45, 34, 56, 67, 45, 23, 18, 15, 155, 123, 1555];
+for( i= 0; i <= 7; i++){
+    var number = numbers[i]
+    console.log(number)
+}
+
+var items = ['bottle', 'mouse', 'pen', 'sunglass', 'book', 'diary', 'paper', 'pencil'];
+for(var i=0; i < items.length; i++){
+    var item = items[i];
+    console.log(item)
+}
